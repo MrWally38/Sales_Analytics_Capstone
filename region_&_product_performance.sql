7ad434b0f3469e8d51221ed7cbc39012ef8b9939
Walter <walterbiochem@gmail.com>
2026-03-12 17:16:30 +0100

    rename & update file to simulate merge conflict

---
+++ region_&_product_performance.sql
@@ -0,0 +1,29 @@
+-- Top Products Analysis
+
+-- Top Product by Sales
+SELECT product_id,
+       product_name,
+       ROUND(SUM(revenue),2) AS total_sales_per_product
+FROM sales_data
+GROUP BY product_id, product_name
+ORDER BY 3 DESC;
+
+-- Top Product by Quantity Sold
+SELECT product_id,
+       product_name,
+       SUM(quantity_sold) AS quantity_sold_per_product
+FROM sales_data
+GROUP BY product_id, product_name
+ORDER BY 3 DESC;
+
+-- Region and Product Performance by Quantity Sold
+-- NB: The Minimum Number of Quantity sold is 6
+
+SELECT product_id,
+       product_name,
+	   region,
+	   SUM(quantity_sold) AS quantity_sold_per_product
+FROM sales_data
+GROUP BY product_id, product_name, region
+HAVING SUM(quantity_sold) >= 6 
+ORDER BY 4 DESC;
